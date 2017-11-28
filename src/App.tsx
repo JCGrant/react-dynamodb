99909e4f8fd4df58e5766c76e40d843ec79c0015
JCGrant <jamescolin.grant@gmail.com>
2017-11-28 11:07:05 +0000

    Post new comments to DynamoDB via API Gateway

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,33 @@ import * as React from 'react';
 
 const commentsEndpoint = 'https://a4hzf96mfg.execute-api.eu-west-2.amazonaws.com/prod/comments';
 
+const getComments = (pageId: string) => {
+  return fetch(commentsEndpoint + '/' + pageId, {
+    headers: new Headers({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }),
+    method: 'GET'
+  })
+  .then((res) => res.json())
+  .then((json) => json.comments);
+};
+
+const newComment = (message: string, pageId: string, userName?: string, ) => {
+  return fetch(commentsEndpoint, {
+    headers: new Headers({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }),
+    method: 'POST',
+    body: JSON.stringify({
+      pageId,
+      userName: userName || 'Anonymous',
+      message,
+    }),
+  });
+};
+
 interface Comment {
   commentId: string;
   message: string;
@@ -23,6 +50,7 @@ class CommentContainer extends React.Component<CommentContainerProps> {
 
 interface PageLocalState {
   comments: Comment[];
+  newCommentText: string;
 }
 interface PageProps {
   id: string;
@@ -32,21 +60,38 @@ class Page extends React.Component<PageProps, PageLocalState> {
     super(props);
     this.state = {
       comments: [],
+      newCommentText: '',
     };
-    fetch(commentsEndpoint + '/' + this.props.id, {
-      headers: new Headers({
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }),
-      method: 'GET'
-    })
-    .then((res) => res.json())
-    .then((json) => this.setState((state) => ({comments: json.comments})));
+    this.setComments();
+  }
+
+  setComments = () => {
+    getComments(this.props.id)
+    .then((comments) => this.setState((state) => ({comments})));
+  }
+
+  onChangeNewCommentInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newCommentText = e.target.value;
+    this.setState((state) => ({
+      newCommentText,
+    }));
+  }
+
+  onClickNewCommentButton = () => {
+    if (this.state.newCommentText.length > 0) {
+      newComment(this.state.newCommentText, this.props.id);
+      this.setComments();
+    }
   }
 
   render() {
     return (
       <div>
+        <input
+          value={this.state.newCommentText}
+          onChange={this.onChangeNewCommentInput}
+        />
+        <button onClick={this.onClickNewCommentButton}>+</button>
         {this.state.comments.map((comment) =>
           <CommentContainer key={comment.commentId} {...comment} />)}
       </div>
