057cd105a60dce4a69e0848fb50dbfbd117289b6
JCGrant <jamescolin.grant@gmail.com>
2017-11-28 10:36:20 +0000

    Display a list of comments from DynamoDB via API Gateway

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,73 @@
 import * as React from 'react';
-import './App.css';
 
-const logo = require('./logo.svg');
+const commentsEndpoint = 'https://a4hzf96mfg.execute-api.eu-west-2.amazonaws.com/prod/comments';
 
-class App extends React.Component {
+interface Comment {
+  commentId: string;
+  message: string;
+  userName: string;
+}
+
+interface CommentContainerProps extends Comment {
+
+}
+class CommentContainer extends React.Component<CommentContainerProps> {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        {this.props.userName}: {this.props.message}
       </div>
+    );
+  }
+}
+
+interface PageLocalState {
+  comments: Comment[];
+}
+interface PageProps {
+  id: string;
+}
+class Page extends React.Component<PageProps, PageLocalState> {
+  constructor(props: PageProps) {
+    super(props);
+    this.state = {
+      comments: [],
+    };
+    fetch(commentsEndpoint + '/' + this.props.id, {
+      headers: new Headers({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }),
+      method: 'GET'
+    })
+    .then((res) => res.json())
+    .then((json) => this.setState((state) => ({comments: json.comments})));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.comments.map((comment) =>
+          <CommentContainer key={comment.commentId} {...comment} />)}
+      </div>
+    );
+  }
+}
+
+interface AppLocalState {
+  currentPageId: string;
+}
+class App extends React.Component<{}, AppLocalState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      currentPageId: 'index',
+    };
+  }
+
+  render() {
+    return (
+      <Page id={this.state.currentPageId} />
     );
   }
 }
